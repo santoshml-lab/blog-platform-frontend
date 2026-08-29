bf47196e8c2574294857cc3b7dde922e9c5e25af
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-29 22:49:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,61 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import PostDetails from "./pages/PostDetails";
+
 function App() {
   return (
-    <div>
-      <h1>BLOG PLATFORM IS WORKING ✅</h1>
-    </div>
+    <Routes>
+
+      {/* Default */}
+      <Route
+        path="/"
+        element={<Navigate to="/login" replace />}
+      />
+
+      {/* Authentication */}
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+      {/* Posts */}
+      <Route
+        path="/home"
+        element={<Home />}
+      />
+
+      <Route
+        path="/create-post"
+        element={<CreatePost />}
+      />
+
+      <Route
+        path="/edit-post/:postId"
+        element={<EditPost />}
+      />
+
+      <Route
+        path="/post/:postId"
+        element={<PostDetails />}
+      />
+
+      {/* Unknown route */}
+      <Route
+        path="*"
+        element={<Navigate to="/login" replace />}
+      />
+
+    </Routes>
   );
 }
 
